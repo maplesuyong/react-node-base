70b644265ab5154a159d37f982b6cf367f91001f
suyong <suyong0507@gmail.com>
2022-12-15 17:00:30 +0900

    로그인, 로그아웃, 인증

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import express from 'express'
 import mongoose from 'mongoose'
 import { User } from './models/User.js'
 import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import { auth } from './middleware/auth.js'
 
 const app = express()
 const PORT = 5000
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use(cookieParser())
 
 mongoose.set('strictQuery', false)
 mongoose.connect('mongodb+srv://root:1q2w3e4r@react-node.pzbh1qm.mongodb.net/?retryWrites=true&w=majority')
@@ -20,10 +23,60 @@ app.post('/register', (req, res) => {
   const user = new User(req.body)
 
   user.save((err, userInfo) => {
+    console.log(userInfo)
     if(err) {
       return res.json({ success: false, err })
     }
     return res.status(200).json({
+      success: true
+    })
+  })
+})
+
+app.post('/login', (req, res) => {
+  // 요청된 이메일이 DB에 있는지 먼저 확인
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        mesaage: "입력된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    // 요청된 이메일이 DB에 있다면 비밀번호가 맞는지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+      }
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err)
+
+        res.cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
+      })
+    })
+  })
+})
+
+app.get('/auth', auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false: true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).send({
       success: true
     })
   })
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const saltRounds = 10
 
@@ -37,19 +38,46 @@ const userSchema = mongoose.Schema({
 // pre: user 스키마에 데이터를 저장하기(몽구스의 save 메소드 실행) 전에 실행하는 메소드
 userSchema.pre('save', function (next) {
   let user = this
-  console.log('function에서 this: ', user)
 
   if (user.isModified('password')) {
-    console.log('isModified 메소드 실행됨')
-    bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
       if (err) return next(err)
-      bcrypt.hash(user.password, salt, function (err, hash) {
+      bcrypt.hash(user.password, salt, (err, hash) => {
         if (err) return next(err)
         user.password = hash
         next()
       })
     })
+  } else {
+    next()
   }
 })
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+    if (err) return cb(err)
+    cb(null, isMatch)
+  })
+}
+
+userSchema.methods.generateToken = function (cb) {
+  let user = this;
+  let token = jwt.sign(user._id.toHexString(), 'SECRET_TOKEN')
+  user.token = token
+  user.save((err, user) => {
+    if (err) return cb(err)
+    cb(null, user)
+  })
+}
+
+userSchema.statics.findByToken = function (token, cb) {
+  let user = this
+  jwt.verify(token, 'SECRET_TOKEN', (err, decoded) => {
+    user.findOne({ "_id": decoded, "token": token }, (err, user) => {
+      if (err) return cb(err)
+      cb(null, user)
+    })
+  })
+}
 
 export const User = mongoose.model('User', userSchema);
